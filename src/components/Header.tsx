c80f06fbc539533c7413d9529a8176da18fe391e
javohir98 <47714181+javohir98@users.noreply.github.com>
2023-03-17 21:21:44 +0500

    header completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,108 @@
 import styled from '@emotion/styled'
 import { Container } from '@mui/material';
-import React from 'react'
+import { useEffect, useState } from 'react';
+import useScrollspy from '../tools/hooks/useScrollspy';
+import { IProductGroup } from '../types/types';
+import CartHeader from './cart-header/CartHeader';
 
-type Props = {}
+type Props = {
+  data: any
+}
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  position: sticky;
+  top: 0;
+  left: 0;
+  background-color: #fff;
+  transition: all .3s ease;
+  border-radius: 0;
+  z-index: 1;
+  /* margin: 25px auto 0; */
+
+  &.sticky {
+    box-shadow: -5px 0 15px rgb(0 0 0 / 20%);
+  }
+`;
+
+const LinkList = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 10px;
+  max-width: 100%;
+  overflow-x: auto;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span {
+    padding: 5px 15px;
+    border-radius: 100px;
+    background: #fbf8ce;
+    transition: all .3s ease;
+    margin: 10px 0;
+    color: #000;
+
+    &.active {
+      background-color: #fcea04!important; 
+    }
+  }
+`;
 
 const Header = (props: Props) => {
+  const { data } = props;
+  const [isSticky, setIsSticky] = useState(false)
+  const ids = data && getIds(data?.data)
+  const activeId = useScrollspy(ids ?? [], 59)
+
+  const listenScrollEvent = () => {
+    const pizza = document.getElementById(`${data?.data[0].id}`) as HTMLElement;
+
+    if(window.scrollY > pizza.offsetTop) {
+      setIsSticky(true)
+    } else {
+      setIsSticky(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+
+    return () => {
+      window.removeEventListener("scroll", listenScrollEvent);
+    };
+  }, [data]);
+
+  function getIds(products: IProductGroup[]) {
+    let arr: string[] = [];
+
+    products?.forEach((item: IProductGroup) => {
+      arr.push(item.id as string)
+    })
+
+    return arr;
+  }
+
   return (
-    <Wrapper>
-        <Container>
-            test
-        </Container>
-    </Wrapper>
+    <>
+      <Wrapper id='header' className={isSticky ? 'sticky' : ''}>
+          <Container style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+            <LinkList>
+              {data?.data?.map((item: IProductGroup) => (
+                <Link href={`#${item.id}`} key={item.id}>
+                  <span className={item?.id === activeId ? 'active' : ''}>{item.name}</span>
+                </Link>
+              ))}
+            </LinkList>
+            <CartHeader />
+          </Container>
+      </Wrapper>
+    </>
   )
 }
 
